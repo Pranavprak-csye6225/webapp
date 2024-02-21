9b2c4774752fb9a34feb3a7e4398e472d414b3a1
Pranav Prakash <prakash.pran@northeastern.edu>
2024-02-21 17:10:32 -0500

    Combined separate shell provisioner to single provisioner

--- packer/script/startup-service.sh
+++ packer/script/startup-service.sh
@@ -2,7 +2,6 @@
 
 sudo mv /tmp/webapp-1.1.0.jar /opt/
 sudo mv /tmp/.env /opt/
-sudo chown -R csye6225:csye6225 /tmp/webapp.service
 sudo mv /tmp/webapp.service /etc/systemd/system/webapp.service
 sudo systemctl daemon-reload
 sudo systemctl enable webapp.service
