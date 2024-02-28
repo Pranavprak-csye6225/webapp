9ac93ceab7b537c1cdd5046e246687d0d1233e90
Pranav Prakash <prakash.pran@northeastern.edu>
2024-02-28 16:28:25 -0500

    Added a path service to restart systemd when path appears

--- packer/script/startup-service.sh
+++ packer/script/startup-service.sh
@@ -2,6 +2,8 @@
 
 sudo mv /tmp/webapp-1.1.0.jar /opt/
 sudo mv /tmp/webapp.service /etc/systemd/system/webapp.service
+sudo mv /tmp/webapp.path /etc/systemd/system/webapp.path
 echo "SELINUX=permissive" | sudo tee /etc/selinux/config
 sudo systemctl daemon-reload
+sudo systemctl enable webapp.path
 sudo systemctl enable webapp.service
