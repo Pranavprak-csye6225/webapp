bcbc32dccb7e8af316f203a7166d0fe54292e11b
Pranav Prakash <prakash.pran@northeastern.edu>
2024-02-28 14:51:50 -0500

    Disabled selinux

--- packer/script/startup-service.sh
+++ packer/script/startup-service.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo mv /tmp/webapp-1.1.0.jar /opt/
-sudo mv /tmp/.env /opt/
 sudo mv /tmp/webapp.service /etc/systemd/system/webapp.service
+echo "SELINUX=permissive" | sudo tee /etc/selinux/config
 sudo systemctl daemon-reload
 sudo systemctl enable webapp.service
