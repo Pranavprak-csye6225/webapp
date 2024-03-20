f85b9007a388b4a607812cceb1b101d3c5c384cd
PranavvPrakash <pranav0715@gmail.com>
2024-03-20 05:21:15 -0400

    Added ops agent

---
+++ packer/script/ops-agent.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+curl -sSO https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo.sh
+sudo bash add-google-cloud-ops-agent-repo.sh --also-install
+sudo touch /var/log/webapp.log
+sudo chown -R csye6225:csye6225 /var/log/webapp.log
+
+
+# Define the YAML content to append
+yaml_content=$(cat <<EOF
+logging:
+  receivers:
+    my-app-receiver:
+      type: files
+      include_paths:
+        - /var/log/webapp.log
+        - /var/log/messages
+      record_log_file_path: true
+  processors:
+    my-app-processor:
+      type: parse_json
+      time_key: "Timestamp/UTC"
+      time_format: "%Y-%m-%dT%H:%M:%S.%LZ"
+    move_severity:
+      type: modify_fields
+      fields:
+        severity:
+          move_from: jsonPayload.severity
+  service:
+    pipelines:
+      default_pipeline:
+        receivers: [my-app-receiver]
+        processors: [my-app-processor, move_severity]
+EOF
+)
+
+# Define the path to the config.yaml file
+config_file="/etc/google-cloud-ops-agent/config.yaml"
+
+# Append YAML content to the config file
+echo "$yaml_content" | sudo tee -a "$config_file" > /dev/null
+echo "YAML content appended to $config_file"
+
+# Restart the Google Cloud Ops Agent
+sudo service google-cloud-ops-agent restart
+
+
