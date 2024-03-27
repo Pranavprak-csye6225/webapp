a7390c7340b8c6cf1032b64dc236acbcc7ce6497
PranavPrak <prakash.pran@northeastern.edu>
2024-03-27 16:57:24 -0400

    Added loggers for exception

--- src/main/java/com/csye6225/webapp/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/csye6225/webapp/exceptions/GlobalExceptionHandler.java
@@ -1,5 +1,8 @@
 package com.csye6225.webapp.exceptions;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.CacheControl;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -19,6 +22,7 @@ import java.util.Collections;
 public class GlobalExceptionHandler {
 
     private final HttpHeaders headers;
+    Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     public GlobalExceptionHandler() {
         this.headers = new HttpHeaders();
@@ -29,47 +33,49 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
     public ResponseEntity<String> handleMethodNotAllowedException() {
+        logger.error("Wrong Method Given");
         return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).headers(headers).build();
     }
 
     @ExceptionHandler(NoResourceFoundException.class)
     public ResponseEntity<String> handleNotFoundException(NoResourceFoundException ex) {
+        logger.error("No Resource Found Exception: "+ex);
         return ResponseEntity.status(HttpStatus.NOT_FOUND).headers(headers).build();
     }
 
     @ExceptionHandler(UserNotCreatedException.class)
     public ResponseEntity<Object> handleUserNotCreatedException(UserNotCreatedException e) {
-        System.out.println("UserNotCreatedException: " + e);
+        logger.error("UserNotCreatedException: " + e);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).headers(headers).body(Collections.singletonMap("error", e.getMessage()));
     }
 
     @ExceptionHandler(UserNotFoundException.class)
     public ResponseEntity<Object> handleUserNotFoundException(UserNotFoundException e) {
-        System.out.println("UserNotFoundException: " + e);
+        logger.error("UserNotFoundException: " + e);
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).headers(headers).body(Collections.singletonMap("error", e.getMessage()));
     }
 
     @ExceptionHandler(UserNotUpdatedException.class)
     public ResponseEntity<Object> handleNotUpdatedException(UserNotUpdatedException e) {
-        System.out.println("handleNotUpdatedException: " + e);
+        logger.error("handleNotUpdatedException: " + e);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).headers(headers).body(Collections.singletonMap("error", e.getMessage()));
     }
 
     @ExceptionHandler(UserNotVerifiedException.class)
     public ResponseEntity<Object> handleNotVerifiedException(UserNotVerifiedException e) {
-        System.out.println("handleNotVerifiedException: " + e);
+        logger.error("handleNotVerifiedException: " + e);
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).headers(headers).body(Collections.singletonMap("verificationStatus", e.getMessage()));
     }
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
     public ResponseEntity<Object> handleWrongRequestException(HttpMessageNotReadableException e) {
-        System.out.println("handleWrongRequestException: " + e);
+        logger.error("handleWrongRequestException: " + e);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).headers(headers).body(Collections.singletonMap("error", "Wrong fields in request"));
     }
 
     @ExceptionHandler(MissingRequestHeaderException.class)
     public ResponseEntity<Object> handleWrongHeadersException(MissingRequestHeaderException e) {
-        System.out.println("handleWrongHeadersException: " + e);
+        logger.error("handleWrongHeadersException: " + e);
         if (e.getHeaderName().equals("authorization")) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).headers(headers).body(Collections.singletonMap("error", "Auth is empty"));
         } else {
@@ -79,13 +85,13 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(HttpMediaTypeNotSupportedException.class)
     public ResponseEntity<String> handleMediaTypeException(HttpMediaTypeNotSupportedException e) {
-        System.out.println("Exception e" + e);
+        logger.error("Exception e" + e);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).headers(headers).build();
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleException(Exception e) {
-        System.out.println("Exception e" + e);
+        logger.error("Exception e" + e);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).headers(headers).build();
     }
 }
